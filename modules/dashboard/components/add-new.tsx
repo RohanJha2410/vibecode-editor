df1b75ed3f8cbf4fb695a91b0cfe61de5d4a5af7
Rohan Jha <jharohan2406@gmail.com>
2025-12-31 01:32:25 +0530

    06-Template Selecting Model

--- modules/dashboard/components/add-new.tsx
+++ modules/dashboard/components/add-new.tsx
@@ -8,6 +8,7 @@ import Image from "next/image"
 import { useRouter } from "next/navigation";
 import { useState } from "react"
 import { toast } from "sonner";
+import TemplateSelectionModal from "./template-selecting-modal";
 
 const AddNewButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -20,15 +21,16 @@ const AddNewButton = () => {
         transition-all duration-300 ease-in-out
         hover:bg-background hover:border-[#E93F3F] hover:scale-[1.02]
         shadow-[0_2px_10px_rgba(0,0,0,0.08)]
-        hover:shadow-[0_10px_30px_rgba(233,63,63,0.15)]"
+        hover:shadow-[0_10px_30px_rgba(233,63,63,0.15)] bg-zinc-800/90 hover:bg-black
+"
       >
         <div className="flex flex-row justify-center items-start gap-4">
           <Button
             variant={"outline"}
-            className="flex justify-center items-center bg-white group-hover:bg-[#fff8f8] group-hover:border-[#E93F3F] group-hover:text-[#E93F3F] transition-colors duration-300"
+            className="flex justify-center items-center bg-white group-hover:bg-[#fff8f8] group-hover:border-[#E93F3F] group-hover:text-[#E93F3F] transition-colors duration-300 bg-zinc-500 border-white/10"
             size={"icon"}
           >
-            <Plus size={30} className="transition-transform duration-300 group-hover:rotate-90" />
+            <Plus size={30} className="transition-transform duration-300 group-hover:rotate-90 " />
           </Button>
           <div className="flex flex-col">
             <h1 className="text-xl font-bold text-[#e93f3f]">Add New</h1>
@@ -46,7 +48,12 @@ const AddNewButton = () => {
           />
         </div>
       </div>
-      
+
+      <TemplateSelectionModal
+      isOpen={isModalOpen}
+      onClose={()=>setIsModalOpen(false)}
+      onSubmit={()=>{}}
+      />
 
     </>
   )
--- modules/dashboard/components/add-repo.tsx
+++ modules/dashboard/components/add-repo.tsx
@@ -10,12 +10,13 @@ const AddRepo = () => {
       transition-all duration-300 ease-in-out
       hover:bg-background hover:border-[#E93F3F] hover:scale-[1.02]
       shadow-[0_2px_10px_rgba(0,0,0,0.08)]
-      hover:shadow-[0_10px_30px_rgba(233,63,63,0.15)]"
+      hover:shadow-[0_10px_30px_rgba(233,63,63,0.15)] bg-zinc-800/90
+hover:bg-black"
     >
       <div className="flex flex-row justify-center items-start gap-4">
         <Button
           variant={"outline"}
-          className="flex justify-center items-center bg-white group-hover:bg-[#fff8f8] group-hover:border-[#E93F3F] group-hover:text-[#E93F3F] transition-colors duration-300"
+          className="flex justify-center items-center bg-white group-hover:bg-[#fff8f8] group-hover:border-[#E93F3F] group-hover:text-[#E93F3F] transition-colors duration-300 bg-zinc-500 border-white/10"
           size={"icon"}
         >
           <ArrowDown size={30} className="transition-transform duration-300 group-hover:translate-y-1" />
